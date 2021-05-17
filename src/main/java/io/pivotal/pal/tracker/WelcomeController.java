e737a2389f307237bf023a70df31d3c241cb9121
Ubuntu <ubuntu@ip-172-31-90-195.ec2.internal>
2021-05-17 18:35:02 +0000

    pre push

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,13 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
 }
